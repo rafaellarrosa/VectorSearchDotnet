476d1958b6aa8014d88ac1fb0710cc8358baeda7
rafabiel <rafael.larrosa@outlook.com>
2025-06-16 17:48:09 -0300

    feat(infrastructure): setup QdrantOptions and Application bootstrap

---
+++ src/Infrastructure/Qdrant/QdrantOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Infrastructure.Qdrant;
+
+public class QdrantOptions
+{
+    public string BaseUrl { get; set; } = "http://localhost:6333";
+    public string CollectionName { get; set; } = "documents";
+}
